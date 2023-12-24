c7c4b251af08a44ce481bb15a41d83032534f867
Jumpei Ogawa <git@phanective.org>
2023-12-24 12:11:49 +0900

    feat: add CLI command to preview and build

---
+++ workspaces/icedkey/src/utils.ts
@@ -0,0 +1,15 @@
+import { exec } from "node:child_process";
+
+const execPromise = async (command: string): Promise<{ stdout: string, stderr: string }> =>
+  new Promise((resolve, reject) =>
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve({ stdout, stderr });
+    }));
+
+export {
+  execPromise as exec,
+};
