3e222bddd15d9c9256fad7ae4f22977198c79c29
Jumpei Ogawa <git@phanective.org>
2023-12-24 12:11:49 +0900

    refactor: extract CSS & JavaScript from HTML
    By extracting JavaScript to .ts file, it requires `dom` lib.

---
+++ workspaces/icedkey/src/app/frontend/index.ts
@@ -0,0 +1,49 @@
+
+const WORKER_URL = "https://github-oauth-login.gr2m.workers.dev";
+const code = new URL(location.href).searchParams.get("code");
+const $login = document.querySelector("#login");
+
+if (code) {
+  login(code);
+}
+
+async function login(code) {
+  // remove ?code=... from URL
+  const path =
+    location.pathname +
+    location.search.replace(/\bcode=\w+/, "").replace(/\?$/, "");
+  history.pushState({}, "", path);
+
+  document.body.dataset.state = "loading";
+
+  try {
+    const response = await fetch(WORKER_URL, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({ code })
+    });
+
+    const result = await response.json();
+
+    if (result.error) {
+      return alert(JSON.stringify(result, null, 2));
+    }
+
+    // token can now be used to send authenticated requests against https://api.github.com
+    const getUserResponse = await fetch("https://api.github.com/user", {
+      headers: {
+        accept: "application/vnd.github.v3+json",
+        authorization: `token ${result.token}`
+      }
+    });
+    const { login } = await getUserResponse.json();
+    $login.textContent = login;
+    document.body.dataset.state = "signed-in";
+  } catch (error) {
+    alert(error);
+    location.reload();
+  }
+}
