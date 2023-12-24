7a868c093e49ae652f13f96efd372cd609abeaa6
Jumpei Ogawa <git@phanective.org>
2023-12-24 12:11:49 +0900

    refactor: set up Hono

---
+++ scripts/prebuild.ts
@@ -0,0 +1,19 @@
+import { writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import originalTsconfig from "../tsconfig.json";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+export type RecursivePartial<T> = {
+  [P in keyof T]?:
+  T[P] extends Array<infer U> ? Array<Value<U>> : Value<T[P]>;
+};
+type AllowedPrimitives = boolean | string | number | Date;
+type Value<T> = T extends AllowedPrimitives ? T : RecursivePartial<T>;
+
+const tsconfig = originalTsconfig as RecursivePartial<typeof originalTsconfig> ;
+delete tsconfig.compilerOptions?.baseUrl;
+delete tsconfig.compilerOptions?.paths;
+
+await writeFile(join(__dirname, "../workspaces/icedkey/tsconfig.json"), JSON.stringify(tsconfig, null, 2));
