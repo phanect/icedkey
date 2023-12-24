9da920f765c8b6c73f850f06d8671ffd239f4051
Jumpei Ogawa <git@phanective.org>
2023-12-24 12:11:49 +0900

    refactor: add typings

--- workspaces/icedkey/src/app/frontend/index.ts
+++ workspaces/icedkey/src/app/frontend/index.ts
@@ -1,7 +1,16 @@
+type LoginResponse = {
+  token: string,
+  error?: string, // TODO check if `error` is really string
+};
 
 const WORKER_URL = "https://github-oauth-login.gr2m.workers.dev";
 const code = new URL(location.href).searchParams.get("code");
 const $login = document.querySelector("#login");
+
+if (!$login) {
+  alert("We're sorry, something technically wrong: error code 101");
+  throw new Error("#login element was not found.");
+}
 
 if (code) {
   // remove ?code=... from URL
@@ -22,7 +31,7 @@ if (code) {
       body: JSON.stringify({ code }),
     });
 
-    const result = await response.json();
+    const result: LoginResponse = await response.json();
 
     if (result.error) {
       alert(JSON.stringify(result, null, 2));
@@ -34,7 +43,7 @@ if (code) {
           authorization: `token ${result.token}`,
         },
       });
-      const { login } = await getUserResponse.json();
+      const { login } = await getUserResponse.json() as { login: string };
       $login.textContent = login;
       document.body.dataset.state = "signed-in";
     }
